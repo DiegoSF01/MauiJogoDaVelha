4b35743e55f4a4bab7670f639611c070fd1b8986
Usuario <Usuario@CPD03-M12>
2024-03-11 15:52:53 -0300

    Jogo da velha em C#

--- MauiJogoDaVelha/MainPage.xaml.cs
+++ MauiJogoDaVelha/MainPage.xaml.cs
@@ -24,55 +24,79 @@
                 vez = "O";
             } //Fecha if
 
+            //Verifica se o X ganhou na 1° linha na horizontal
             if (btn10.Text == "X"
                 && btn11.Text == "X"
                 && btn12.Text == "X")
             {
                 DisplayAlert("Parabéns!", "O X ganhou!", "OK");
                 limpar();
-            }else if (btn20.Text == "X"
+            }
+
+            //Verifica se o X ganhou na 2° linha na horizontal
+            else if (btn20.Text == "X"
                 && btn21.Text == "X"
                 && btn22.Text == "X")
             {
                 DisplayAlert("Parabéns!", "O X ganhou!", "OK");
                 limpar();
-            }else if (btn30.Text == "X"
+            }
+
+            //Verifica se o X ganhou na 3° linha na horizontal
+            else if (btn30.Text == "X"
                 && btn31.Text == "X"
                 && btn32.Text == "X")
             {
                 DisplayAlert("Parabéns!", "O X ganhou!", "OK");
                 limpar();
-            }else if (btn10.Text == "X"
+            }
+
+            //Verifica se o X ganhou na diagonal esquerda para direita
+            else if (btn10.Text == "X"
                 && btn21.Text == "X"
                 && btn32.Text == "X")
             {
                 DisplayAlert("Parabéns!", "O X ganhou!", "OK");
                 limpar();
-            }else if (btn12.Text == "X"
+            }
+
+            //Verifica se o X ganhou na diagonal direita para esquerda
+            else if (btn12.Text == "X"
                 && btn21.Text == "X"
                 && btn30.Text == "X")
             {
                 DisplayAlert("Parabéns!", "O X ganhou!", "OK");
                 limpar();
-            }else if (btn10.Text == "X"
+            }
+
+            //Verifica se o X ganhou na 1° linha da vertical
+            else if (btn10.Text == "X"
                 && btn20.Text == "X"
                 && btn30.Text == "X")
             {
                 DisplayAlert("Parabéns!", "O X ganhou!", "OK");
                 limpar();
-            }else if (btn11.Text == "X"
+            }
+
+            //Verifica se o X ganhou na 2° linha da vertical
+            else if (btn11.Text == "X"
                 && btn21.Text == "X"
                 && btn31.Text == "X")
             {
                 DisplayAlert("Parabéns!", "O X ganhou!", "OK");
                 limpar();
-            }else if (btn12.Text == "X"
+            }
+
+            //Verifica se o X ganhou na 3° linha da vertical
+            else if (btn12.Text == "X"
                 && btn22.Text == "X"
                 && btn32.Text == "X")
             {
                 DisplayAlert("Parabéns!", "O X ganhou!", "OK");
                 limpar();
             }
+
+            //Verifica se a O ganhou na 1° linha da horizontal
             else if (btn10.Text == "O"
                 && btn11.Text == "O"
                 && btn12.Text == "O")
@@ -80,6 +104,8 @@
                 DisplayAlert("Parabéns!", "O O ganhou!", "OK");
                 limpar();
             }
+
+            //Verifica se a O ganhou na 2° linha da horizontal
             else if (btn20.Text == "O"
                 && btn21.Text == "O"
                 && btn22.Text == "O")
@@ -87,6 +113,8 @@
                 DisplayAlert("Parabéns!", "O O ganhou!", "OK");
                 limpar();
             }
+
+            //Verifica se a O ganhou na 3° linha da horizontal
             else if (btn30.Text == "O"
                 && btn31.Text == "O"
                 && btn32.Text == "O")
@@ -94,6 +122,8 @@
                 DisplayAlert("Parabéns!", "O O ganhou!", "OK");
                 limpar();
             }
+
+            //Verifica se a O ganhou na diagonal esquerda para direita
             else if (btn10.Text == "O"
                 && btn21.Text == "O"
                 && btn32.Text == "O")
@@ -101,6 +131,8 @@
                 DisplayAlert("Parabéns!", "O O ganhou!", "OK");
                 limpar();
             }
+
+            //Verifica se a O ganhou na diagonal direita para esquerda
             else if (btn12.Text == "O"
                 && btn21.Text == "O"
                 && btn30.Text == "O")
@@ -108,6 +140,8 @@
                 DisplayAlert("Parabéns!", "O O ganhou!", "OK");
                 limpar();
             }
+
+            //Verifica se a O ganhou na 1° linha da vertical
             else if (btn10.Text == "O"
                 && btn20.Text == "O"
                 && btn30.Text == "O")
@@ -115,6 +149,8 @@
                 DisplayAlert("Parabéns!", "O X ganhou!", "OK");
                 limpar();
             }
+
+            //Verifica se a O ganhou na 2° linha da vertical
             else if (btn11.Text == "O"
                 && btn21.Text == "O"
                 && btn31.Text == "O")
@@ -122,6 +158,8 @@
                 DisplayAlert("Parabéns!", "O O ganhou!", "OK");
                 limpar();
             }
+
+            //Verifica se a O ganhou na 3° linha da vertical
             else if (btn12.Text == "O"
                 && btn22.Text == "O"
                 && btn32.Text == "O")
