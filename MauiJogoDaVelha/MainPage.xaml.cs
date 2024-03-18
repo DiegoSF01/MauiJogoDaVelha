ba1a8dd4806a607ac8b76790d285d8ba7f1ddee3
Usuario <Usuario@CPD03-M12>
2024-03-18 15:41:02 -0300

    Jogo da velha em C#

--- MauiJogoDaVelha/MainPage.xaml.cs
+++ MauiJogoDaVelha/MainPage.xaml.cs
@@ -168,13 +168,56 @@
                 limpar();
             }
 
+            //Verifica se deu empate(velha)
+            else if (btn10.Text == "X" ^ btn10.Text == "O" &&
+                   btn11.Text == "X" ^ btn11.Text ==  "O" &&
+                   btn12.Text == "X" ^ btn12.Text == "O" &&
+                   btn20.Text == "X" ^ btn20.Text == "O" &&
+                   btn21.Text == "X" ^ btn21.Text == "O" &&
+                   btn22.Text == "X" ^ btn22.Text == "O" &&
+                   btn30.Text == "X" ^ btn30.Text == "O" &&
+                   btn31.Text == "X" ^ btn31.Text == "O" &&
+                   btn32.Text == "X" ^ btn32.Text == "O")
+
+
+            {
+                DisplayAlert("Caramba!", "Deu velha!", "OK");
+                limpar();
+            }
+
         } //Fecha Button_Clicked
 
         private void limpar()
         {
             btn10.Text = "";
             btn10.IsEnabled = true;
+
+            btn11.Text = "";
+            btn11.IsEnabled = true;
+
+            btn12.Text = "";
+            btn12.IsEnabled = true;
+
+            btn20.Text = "";
+            btn20.IsEnabled = true;
+
+            btn21.Text = "";
+            btn21.IsEnabled = true;
+
+            btn22.Text = "";
+            btn22.IsEnabled = true;
+
+            btn30.Text = "";
+            btn30.IsEnabled = true;
+
+            btn31.Text = "";
+            btn31.IsEnabled = true;
+
+            btn32.Text = "";
+            btn32.IsEnabled = true;
+
         }
+
     }
 
 }
